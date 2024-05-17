39b357779c2b4f4dea7f02fbcc04adfe68165d75
HaPham <ha.phamhong@amela.vn>
2024-05-17 17:30:24 +0700

    hlt

--- src/Component/Post.js
+++ src/Component/Post.js
@@ -1,16 +1,30 @@
 import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
-import { Avatar } from "@mui/material";
+import { Avatar  } from "@mui/material";
+import { CardMedia } from "@mui/material";
 import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import "./Post.css";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import ChatBubbleOutlineIcon from "@mui/icons-material/ChatBubbleOutline";
 import TelegramIcon from "@mui/icons-material/Telegram";
 import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
-
+import Card from '@mui/material/Card';
+import videoURL from "./video.mp4";
 function Post({ user, postImage, likes, timestamp }) {
   const ariaLabel = { 'aria-label': 'description' };
-
+  const postVideo = require("./video.mp4").default;
+  const data = [1,2,3,4]
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
   return (
+    <>
     <div className="post">
       <div className="post__header">
         <div className="post__headerAuthor">
@@ -21,7 +35,13 @@ function Post({ user, postImage, likes, timestamp }) {
         <MoreHorizIcon />
       </div>
       <div className="post__image">
-        <img src={postImage} alt="Post Image" width={468} height={585}/>
+       
+        <Slider {...settings}>
+      {data.map(() => (
+         <img src={postImage} alt="Post Image" width={468} height={585}/>
+      ))}
+    </Slider>
+     
       </div>
       <div className="post__footer">
         <div className="post__footerIcons">
@@ -38,6 +58,41 @@ function Post({ user, postImage, likes, timestamp }) {
        
       </div>
     </div>
+    <div className="post">
+      <div className="post__header">
+        <div className="post__headerAuthor">
+          <Avatar style={{ marginRight: "10px" }}>
+          </Avatar>
+          {user} • <span>{timestamp}</span>
+        </div>
+        <MoreHorizIcon />
+      </div>
+      <div className="post__image">
+      <CardMedia
+        component="video"
+        src={videoURL}
+        controls
+        allow="autoPlay"
+        width={468}
+        height={585}
+      />
+      </div>
+      <div className="post__footer">
+        <div className="post__footerIcons">
+          <div className="post__iconsMain">
+            <FavoriteBorderIcon className="postIcon" />
+            <ChatBubbleOutlineIcon className="postIcon" />
+            <TelegramIcon className="postIcon" />
+          </div>
+          <div className="post__iconSave">
+            <BookmarkBorderIcon className="postIcon" />
+          </div>
+        </div>
+       <b>38,250 likes</b> 
+       
+      </div>
+    </div>
+    </>
   );
 }
 
