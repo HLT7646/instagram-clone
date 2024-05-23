d1f3136eb5f41493f0768f31897820492119851f
HaPham <ha.phamhong@amela.vn>
2024-05-23 14:46:44 +0700

    icon

--- src/Component/Post.js
+++ src/Component/Post.js
@@ -1,21 +1,25 @@
-import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
-import { Avatar  } from "@mui/material";
-import { CardMedia } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
+import { Avatar, CardMedia } from "@mui/material";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./Post.css";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import FavoriteIcon from "@mui/icons-material/Favorite";
 import ChatBubbleOutlineIcon from "@mui/icons-material/ChatBubbleOutline";
 import TelegramIcon from "@mui/icons-material/Telegram";
 import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
-import Card from '@mui/material/Card';
-import videoURL from "./video.mp4";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+
 function Post({ user, postImage, likes, timestamp }) {
+  const [isLiked, setIsLiked] = useState(false);
+
+  const handleLikeClick = () => {
+    setIsLiked(!isLiked);
+  };
+
   const ariaLabel = { 'aria-label': 'description' };
-  const postVideo = require("./video.mp4").default;
-  const data = [1,2,3,4]
+  const data = [1, 2, 3, 4];
   const settings = {
     dots: true,
     infinite: true,
@@ -23,75 +27,45 @@ function Post({ user, postImage, likes, timestamp }) {
     slidesToShow: 1,
     slidesToScroll: 1,
   };
+
   return (
     <>
-    <div className="post">
-      <div className="post__header">
-        <div className="post__headerAuthor">
-          <Avatar style={{ marginRight: "10px" }}>
-          </Avatar>
-          {user} • <span>{timestamp}</span>
-        </div>
-        <MoreHorizIcon />
-      </div>
-      <div className="post__image">
-       
-        <Slider {...settings}>
-      {data.map(() => (
-         <img src={postImage} alt="Post Image" width={468} height={585}/>
-      ))}
-    </Slider>
-     
-      </div>
-      <div className="post__footer">
-        <div className="post__footerIcons">
-          <div className="post__iconsMain">
-            <FavoriteBorderIcon className="postIcon" />
-            <ChatBubbleOutlineIcon className="postIcon" />
-            <TelegramIcon className="postIcon" />
+      <div className="post">
+        <div className="post__header">
+          <div className="post__headerAuthor">
+            <Avatar style={{ marginRight: "10px" }}></Avatar>
+            {user} • <span>{timestamp}</span>
           </div>
-          <div className="post__iconSave">
-            <BookmarkBorderIcon className="postIcon" />
-          </div>
+          <MoreHorizIcon />
         </div>
-       <b>38,250 likes</b> 
-       
-      </div>
-    </div>
-    <div className="post">
-      <div className="post__header">
-        <div className="post__headerAuthor">
-          <Avatar style={{ marginRight: "10px" }}>
-          </Avatar>
-          {user} • <span>{timestamp}</span>
+        <div className="post__image">
+          <Slider {...settings}>
+            {data.map(() => (
+              <img src={postImage} alt="Post Image" width={468} height={585} />
+            ))}
+          </Slider>
         </div>
-        <MoreHorizIcon />
-      </div>
-      <div className="post__image">
-      <CardMedia
-        component="video"
-        src={videoURL}
-        controls
-        allow="autoPlay"
-        width={468}
-        height={585}
-      />
-      </div>
-      <div className="post__footer">
-        <div className="post__footerIcons">
-          <div className="post__iconsMain">
-            <FavoriteBorderIcon className="postIcon" />
-            <ChatBubbleOutlineIcon className="postIcon" />
-            <TelegramIcon className="postIcon" />
+        <div className="post__footer">
+          <div className="post__footerIcons">
+            <div className="post__iconsMain">
+              {isLiked ? (
+                <FavoriteIcon className="postIcon" onClick={handleLikeClick} />
+              ) : (
+                <FavoriteBorderIcon
+                  className="postIcon"
+                  onClick={handleLikeClick}
+                />
+              )}
+              <ChatBubbleOutlineIcon className="postIcon" />
+              <TelegramIcon className="postIcon" />
+            </div>
+            <div className="post__iconSave">
+              <BookmarkBorderIcon className="postIcon" />
+            </div>
           </div>
-          <div className="post__iconSave">
-            <BookmarkBorderIcon className="postIcon" />
-          </div>
+          <b>38,250 likes</b>
         </div>
-       <b>38,250 likes</b> 
-       
       </div>
-    </div>
     </>
   );
 }
